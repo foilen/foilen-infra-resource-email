38195133c3ca676081269e0e94bb8182da5a0e24
Simon Levesque <simon@foilen.com>
2018-10-01 21:47:41 -0400

    Update fcloud-docker-email to 3.1.0-3

--- src/main/java/com/foilen/infra/resource/email/handlers/JamesEmailServerEventHandler.java
+++ src/main/java/com/foilen/infra/resource/email/handlers/JamesEmailServerEventHandler.java
@@ -168,7 +168,7 @@ public class JamesEmailServerEventHandler extends AbstractFinalStateManagedResou
             application.setApplicationDefinition(applicationDefinition);
             applicationDefinition.setRunAs(unixUserId);
 
-            applicationDefinition.setFrom("foilen/fcloud-docker-email:3.1.0-2");
+            applicationDefinition.setFrom("foilen/fcloud-docker-email:3.1.0-3");
 
             // Start script
             IPApplicationDefinitionAssetsBundle assetsBundle = applicationDefinition.addAssetsBundle();
